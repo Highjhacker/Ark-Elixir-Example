9b39f940b88cfdeafbab8a4e9ff1a641a738c6d7
Highjhacker <mc-somango@live.fr>
2017-11-08 02:45:15 +0100

    Refactored for Ark_elixir 0.1.2

--- lib/ark_elixir_example.ex
+++ lib/ark_elixir_example.ex
@@ -1,14 +1,23 @@
 defmodule ArkElixirExample do
+
     def main(argv) do
-        {options, _, _} = OptionParser.parse(argv,
-            switches: [address: :string],
-            aliases: [a: :address]
-        )
-        try do
-            IO.inspect Ark_Elixir.Account.get_balance(options[:address])
-        rescue
-            e in RuntimeError -> e
-            IO.puts e.message
-        end
+        argv
+        |> parse_args
     end
+
+    def parse_args(args) do
+        parsed_args = OptionParser.parse(args, switches: [help: :boolean, address: :string, search: :string],
+                                            aliases: [h: :help, a: :address, s: :search])
+           case parsed_args do
+             {[help: true], _, _} -> help()
+             {[address: address], _, _} -> IO.inspect Ark_Elixir.Account.get_balance(address)
+             {[search: query], _, _} -> IO.inspect Ark_Elixir.Delegate.search_delegates([q: query])
+             _ -> :help
+           end
+    end
+
+    def help do
+        IO.puts "Help of ArkElixirExample !\n-h --help for help\n-a --address validArkAddress for watching the balance of an account\n-s --search 'delegate' for searching a delegate."
+    end
+
 end
